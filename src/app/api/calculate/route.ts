db67d2e9472cfddde096e7806dd2cc96cae8ef3e
gordiash <jaroslaw.jakubczyk@gmail.com>
2025-06-13 18:07:30 +0200

    update

--- src/app/api/calculate/route.ts
+++ src/app/api/calculate/route.ts
@@ -861,8 +861,9 @@ export async function OPTIONS() {
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 if (process.env.JEST_WORKER_ID) {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const mod = module as any;
+  const mod = module as NodeJS.Module & {
+    exports: Record<string, unknown>;
+  };
   mod.exports.handlePurchaseCalculation = handlePurchaseCalculation;
   mod.exports.handleRentalCalculation = handleRentalCalculation;
   mod.exports.handleCreditScoreCalculation = handleCreditScoreCalculation;
