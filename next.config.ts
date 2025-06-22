4b87744ef61ac24f264cc982e26f23469f455d58
gordiash <jaroslaw.jakubczyk@gmail.com>
2025-06-22 10:38:51 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   // Optymalizacje kompilacji
@@ -26,6 +27,12 @@ const nextConfig: NextConfig = {
       };
     }
     
+    // Alias '@' -> ./src for both server & client bundles
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+    };
+
     return config;
   },
 
