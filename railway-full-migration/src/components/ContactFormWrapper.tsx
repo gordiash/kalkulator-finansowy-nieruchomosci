fe5732382827fa058d6f043b4389ff6f7437147f
gordiash <jaroslaw.jakubczyk@gmail.com>
2025-08-05 17:40:20 +0200

    Uporzadkuj projekt - usun niepotrzebne pliki i katalogi

--- railway-full-migration/src/components/ContactFormWrapper.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import dynamic from 'next/dynamic'
-
-const ContactForm = dynamic(() => import('./ContactForm'))
-
-export default function ContactFormWrapper() {
-  return <ContactForm />
-} 
